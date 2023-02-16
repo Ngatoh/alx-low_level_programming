110f4a3beb6124636612ecddb60e3aabcd717835
Ngatoh <alexanderngatia@gmail.com>
2023-02-16 10:34:22 -0800

    Initial commit 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+/*
+ * File: 4-puts.c
+ * Auth: Alex
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "Programming is like
+ * building a multilingual puzzle" using the puts function and returns 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
