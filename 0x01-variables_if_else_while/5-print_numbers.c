ae98949fdaf85dd99a705b4d91d7cf83396a8a0f
Ngatoh <alexanderngatia@gmail.com>
2023-02-17 13:52:04 -0800

    initial commit 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Alex
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int num = 0;
+
+  while (num < 10)
+  {
+    putchar(num + '0');
+    num++;
+  }
+
+  putchar('\n');
+
+  return (0);
+}
