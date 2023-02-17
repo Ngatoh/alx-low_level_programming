4b45d1726afad6e88147df911d91d9342f3f221f
Ngatoh <alexanderngatia@gmail.com>
2023-02-17 14:09:46 -0800

    Initial commit 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Brennan D Baraban
+ */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int num = 0;
+
+  while (num < 10)
+  {
+    putchar(num + '0');
+    num++;
+  }
+
+  putchar('\n');
+
+  return (0);
+}
