31edb965c8d9ca40d445c9d1da85d0fc92de1cb7
Ngatoh <alexanderngatia@gmail.com>
2023-02-17 13:26:49 -0800

    Initial commit3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Alex
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char letter = 'a';
+
+  while (letter <= 'z')
+  {
+    putchar(letter);
+    letter++;
+  }
+
+  letter = 'A';
+
+  while (letter <= 'Z')
+  {
+    putchar(letter);
+    letter++;
+  }
+
+  putchar('\n');
+
+  return (0);
+}
