7ef5ee90e2d4483037934188ca70cf727e9a9a18
Ngatoh <alexanderngatia@gmail.com>
2023-02-17 14:33:02 -0800

    Initial commit 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,43 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Alex
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i = 0;
+
+  while (i < 10)
+  {
+    int j = i + 1;
+
+    while (j < 10)
+    {
+      putchar(i + '0');
+      putchar(',');
+      putchar(' ');
+      putchar(j + '0');
+
+      if (i != 8 || j != 9)
+      {
+        putchar(',');
+        putchar(' ');
+      }
+
+      j++;
+    }
+
+    i++;
+  }
+
+  putchar('\n');
+
+  return (0);
+}
