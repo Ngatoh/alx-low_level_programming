f1a30c314c95534322807680dd300497137bf361
Ngatoh <alexanderngatia@gmail.com>
2023-02-17 16:13:08 -0800

    update 5-print_numbers.c comments

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,15 +12,12 @@
  */
 int main(void)
 {
-  int num = 0;
-
-  while (num < 10)
-  {
-    putchar(num + '0');
-    num++;
-  }
-
-  putchar('\n');
-
-  return (0);
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+putchar('\n');
+return (0);
 }
