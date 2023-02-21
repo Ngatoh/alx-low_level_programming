ca1084b527ca66da6b7637ee803815a34ae2da67
Ngatoh <alexanderngatia@gmail.com>
2023-02-21 10:37:53 -0800

    Changed alignment 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,30 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (result <= 9)
-                    _putchar(' ');
-            }
-            if (result > 9)
-            {
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            else
-            {
-                _putchar(result + '0');
-            }
-        }
-        _putchar('\n');
-    }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+result = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+if (result <= 9)
+_putchar(' ');
+}
+if (result > 9)
+{
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+else
+{
+_putchar(result + '0');
+}
+}
+_putchar('\n');
+}
 }
