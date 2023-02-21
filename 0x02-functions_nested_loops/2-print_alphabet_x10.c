89faa1ca314ac79caf42b9799196d5ca56999b6b
Ngatoh <alexanderngatia@gmail.com>
2023-02-21 12:09:02 -0800

    Changed alignment 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-    int i, letter;
+int i, letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
+for (i = 0; i < 10; i++)
+{
+letter = 'a';
 
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
 
-        _putchar('\n');
-    }
+_putchar('\n');
+}
 }
