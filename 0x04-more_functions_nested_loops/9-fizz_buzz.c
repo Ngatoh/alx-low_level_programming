597abe9efec2240d1e7670de7160c3303323f9a0
Ngatoh <alexanderngatia@gmail.com>
2023-02-23 13:30:28 -0800

    Corrected spacing 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 /**
- * print_fizzbuzz - print the numbers from 1 to 100, replacing multiples of 3 with Fizz,
+ * print_fizzbuzz - print the numbers from 1 to 100,
+ * replacing multiples of 3 with Fizz,
  * multiples of 5 with Buzz, and multiples of both 3 and 5 with FizzBuzz.
  */
 void print_fizzbuzz(void)
 {
 int i;
-
-for (i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz ");
-} else if (i % 3 == 0) {
+}
+else if (i % 3 == 0)
+{
 printf("Fizz ");
-} else if (i % 5 == 0) {
+}
+else if (i % 5 == 0)
+{
 printf("Buzz ");
-} else {
+}
+else
+{
 printf("%d ", i);
 }
-if (i == 100){
+if (i == 100)
+{
 continue;
 printf(" ");
 }
