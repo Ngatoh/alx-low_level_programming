d8b1a8be395345cd5bcd38fa231c29923c1e2875
Ngatoh <alexanderngatia@gmail.com>
2023-02-23 14:01:24 -0800

    Corrected spaces 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,31 +8,31 @@
  */
 int mul(int a, int b)
 {
-    int result = 0;
-    int sign = 1;
+int result = 0;
+int sign = 1;
 
-    /* Handle the sign of the result */
-    if (a < 0)
-    {
-        sign = -sign;
-        a = -a;
-    }
-    if (b < 0)
-    {
-        sign = -sign;
-        b = -b;
-    }
+/* Handle the sign of the result */
+if (a < 0)
+{
+sign = -sign;
+a = -a;
+}
+if (b < 0)
+{
+sign = -sign;
+b = -b;
+}
 
-    /* Compute the product using repeated addition */
-    while (b > 0)
-    {
-        result += a;
-        b--;
-    }
+/* Compute the product using repeated addition */
+while (b > 0)
+{
+result += a;
+b--;
+}
 
-    /* Adjust the sign of the result */
-    if (sign < 0)
-        result = -result;
+/* Adjust the sign of the result */
+if (sign < 0)
+result = -result;
 
-    return result;
+return (result);
 }
