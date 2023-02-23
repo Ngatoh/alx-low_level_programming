a9bbe267277701cc9c3ed13fd8556b4a3206c5f0
Ngatoh <alexanderngatia@gmail.com>
2023-02-23 11:10:36 -0800

    Initial commit 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_fizz_buzz - prints FizzBuzz up to a given number
+ * @n: the maximum number to print
+ *
+ * Return: void
+ */
+void print_fizz_buzz(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_fizz_buzz(100);
+	return (0);
+}
