cea0475a2cbb5411487195596665fdfdf6a33063
Ngatoh <alexanderngatia@gmail.com>
2023-02-23 14:26:56 -0800

    corrected spaces 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,16 @@
  */
 void print_most_numbers(void)
 {
-    int num;
+int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        if (num != 2 && num != 4)
-        {
-            _putchar(num + '0');
-        }
-    }
+for (num = 0; num <= 9; num++)
+{
+if (num != 2 && num != 4)
+{
+_putchar(num + '0');
+}
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
 
