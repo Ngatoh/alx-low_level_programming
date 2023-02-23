990068f9f556997e9e6df2fa88a32aa3100fa5d6
Ngatoh <alexanderngatia@gmail.com>
2023-02-23 14:45:06 -0800

    Corrected spaces 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,24 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            _putchar(' ');
-        }
-        for (j = 0; j <= i; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
