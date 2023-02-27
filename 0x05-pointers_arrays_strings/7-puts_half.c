75fd6fd1c68262f1c36c64aa2af8f196b5a7ca63
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 03:19:48 -0800

    Initial commit 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to string
+ */
+
+void puts_half(char *str)
+{
+    int len = 0;
+    int i, n;
+
+    while (*(str + len) != '\0')
+        len++;
+
+    if (len % 2 == 0)
+        n = len / 2;
+    else
+        n = (len + 1) / 2;
+
+    for (i = n; i < len; i++)
+        _putchar(*(str + i));
+
+    _putchar('\n');
+}
