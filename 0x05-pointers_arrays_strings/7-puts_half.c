74d1ae640115308da5fb1a3c417021adc54e1bda
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 05:24:02 -0800

    removed white spaces

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,19 @@
 
 void puts_half(char *str)
 {
-    int len = 0;
-    int i, n;
+int len = 0;
+int i, n;
 
-    while (*(str + len) != '\0')
-        len++;
+while (*(str + len) != '\0')
+len++;
 
-    if (len % 2 == 0)
-        n = len / 2;
-    else
-        n = (len + 1) / 2;
+if (len % 2 == 0)
+n = len / 2;
+else
+n = (len + 1) / 2;
 
-    for (i = n; i < len; i++)
-        _putchar(*(str + i));
+for (i = n; i < len; i++)
+_putchar(*(str + i));
 
-    _putchar('\n');
+_putchar('\n');
 }
