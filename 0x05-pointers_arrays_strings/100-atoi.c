745d30e5133a62545612ff55dd85b43a8f7e89b7
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 03:48:05 -0800

    Corrected output errors

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string to convert
@@ -21,6 +20,11 @@ int _atoi(char *s)
         else if (*s >= '0' && *s <= '9')
         {
             digit = *s - '0';
+            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
+            {
+                num = (sign == 1) ? INT_MAX : INT_MIN;
+                break;
+            }
             num = num * 10 + digit;
         }
         else if (num > 0)
