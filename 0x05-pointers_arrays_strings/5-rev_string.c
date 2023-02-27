4076cc3a6c9ae22cde0c1c8cd4dbca2e6fdc3c3e
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 02:47:40 -0800

    Corrected errors 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@ void rev_string(char *s)
 {
   int len = 0;
   char tmp;
+  int i = 0;
 
   /* Get length of string */
   while (*(s + len))
     len++;
 
   /* Swap characters from opposite ends of string */
-  for (int i = 0; i < len / 2; i++)
+  for (i = 0; i < len / 2; i++)
   {
     tmp = *(s + i);
     *(s + i) = *(s + len - i - 1);
