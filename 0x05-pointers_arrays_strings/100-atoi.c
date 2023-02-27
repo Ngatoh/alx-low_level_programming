ca3d0f741fed41a477b3408239bf893b9a2144e6
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 04:42:28 -0800

    removed Spaces

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,32 +8,31 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int num = 0;
-    int digit;
+int sign = 1;
+int num = 0;
+int digit;
 
-    while (*s)
-    {
-        if (*s == '-')
-        {
-            sign *= -1;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
-            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
-            {
-                num = (sign == 1) ? INT_MAX : INT_MIN;
-                break;
-            }
-            num = num * 10 + digit;
-        }
-        else if (num > 0)
-        {
-            break;
-        }
-        s++;
-    }
-
-    return (num * sign);
+while (*s)
+{
+if (*s == '-')
+{
+sign *= -1;
+}
+else if (*s >= '0' && *s <= '9')
+{
+digit = *s - '0';
+else if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
+{
+num = (sign == 1) ? INT_MAX : INT_MIN;
+break;
+}
+num = num * 10 + digit;
+}
+else if (num > 0)
+{
+break;
+}
+s++;
+}
+return (num * sign);
 }
