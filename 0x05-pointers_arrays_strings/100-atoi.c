93464797ec64f8cd6adb1e9a4041b0696b0b9fa1
Ngatoh <alexanderngatia@gmail.com>
2023-02-27 04:55:56 -0800

    removed white spaces

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,24 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int digit;
+int sign = 1;
+int result = 0;
+int digit;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
-            result = result * 10 + sign * digit;
-        }
-        else if (result != 0)
-            break;
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+{
+digit = *s - '0';
+result = result * 10 + sign * digit;
+}
+else if (result != 0)
+break;
 
-        s++;
-    }
+s++;
+}
 
-    return result;
+return result;
 }
