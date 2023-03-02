7c1eea4a03be806e95ab1a356b4fd9a98d8bb2d6
Ngatoh <alexanderngatia@gmail.com>
2023-03-02 05:07:43 -0800

    Initial commit 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
